3fcc35006f86d5b51b90f764a752b8601ef64912
Pavol Zbell <pavol.zbell@gmail.com>
2015-10-15 06:09:33 +0200

    Add fixture unpacker, fix archives

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-Dir.glob(File.expand_path '../fixtures/*.tgz', __FILE__).each { |f| `tar -xf #{f} && mv #{File.basename f, '.tgz'} spec/fixtures` unless File.exists? f[0..-5] }
+system File.expand_path '../fixtures/unpack', __FILE__
 Dir.glob(File.expand_path '../support/*.rb', __FILE__).each { |f| require_relative f }
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bump'
