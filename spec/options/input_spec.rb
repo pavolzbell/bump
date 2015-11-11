3726e2a9accd3d8b80546a9a069f2bf6e318addd
Pavol Zbell <pavol.zbell@gmail.com>
2015-11-11 01:47:52 +0100

    Simplify input spec

--- spec/options/input_spec.rb
+++ spec/options/input_spec.rb
@@ -14,61 +14,61 @@ command 'bump' do
       it 'exists on empty file' do
         input = '%/inputs/gem-empty-file.rb'
         stderr = "#{fix}unable to resolve #{relative_path input}"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on missing module' do
         input = '%/inputs/gem-missing-module.rb'
         stderr = "#{fix}unable to resolve #{relative_path input}"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on incorrect module name' do
         input = '%/inputs/gem-incorrect-module-name.rb'
         stderr = "#{fix}unable to resolve #{relative_path input}"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on missing constant name' do
         input = '%/inputs/gem-missing-constant.rb'
         stderr = "#{fix}unable to resolve #{relative_path input}"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on incorrect constant name' do
         input = '%/inputs/gem-incorrect-constant-name.rb'
         stderr = "#{fix}unable to resolve #{relative_path input}"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on 2 version numbers' do
         input = '%/inputs/gem-2-version-numbers.rb'
         stderr = "#{fix}invalid version data: 1,2"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on 5 version nubmers' do
         input = '%/inputs/gem-5-version-numbers.rb'
         stderr = "#{fix}invalid version data: 1,2,3,4,5"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
-      it 'exists on missing major number' do
+      it 'exists on missing number' do
         input = '%/inputs/gem-missing-major-number.rb'
         stderr = "#{fix}invalid version data: ,2,3"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on missing minor number' do
         input = '%/inputs/gem-missing-minor-number.rb'
         stderr = "#{fix}invalid version data: 1,,3"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
 
       it 'exists on missing patch number' do
         input = '%/inputs/gem-missing-patch-number.rb'
         stderr = "#{fix}invalid version data: 1,2"
-        expect(bump "--input '#{expand_path input}' major").to fail_with(stderr: stderr)
+        expect(bump "--input '#{expand_path input}'").to fail_with(stderr: stderr)
       end
     end
   end
